68a4695b50c0e41d8e18c19cd3e91b1a1263fef5
ndsasu <sampo.suokuisma@tuni.fi>
2023-05-28 18:45:19 +0300

    part1 completed

---
+++ osa1/anekdootit/src/App.js
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Uint8Array(anecdotes.length))
+  var mostVotes = Math.max(...votes)
+  var index = votes.indexOf(mostVotes);
+
+  const generateValue = anecdotes => {
+    const random = Math.floor(Math.random() * anecdotes)
+    setSelected(random)
+  }
+
+  const addVote = anecdote => {
+    const copy = [...votes]
+    copy[anecdote] += 1
+    setVotes(copy)
+  }
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <div>{anecdotes[selected]}</div>
+      <div>votes {votes[selected]}</div>
+      <div>
+        <Button handleClick={() => addVote(selected)} text="vote" /> 
+        <Button handleClick={() => generateValue(anecdotes.length)} text="next anecdote" />
+      </div>
+      <h1>Anecdote with the most votes</h1>
+      <div>{anecdotes[index]}</div>
+      <div>has {mostVotes} votes</div>
+    </div>
+  )
+}
+
+export default App;
